087847c146ed9c980fff374e0245a741d94efb82
Steina <steina.dogg@gmail.com>
2018-02-08 01:33:43 +0000

    Initial commit

---
+++ src/main/java/is/hi/Alicia/AliciaApplication.java
@@ -0,0 +1,12 @@
+package is.hi.Alicia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AliciaApplication.class, args);
+	}
+}
---
+++ src/test/java/is/hi/Alicia/AliciaApplicationTests.java
@@ -0,0 +1,16 @@
+package is.hi.Alicia;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AliciaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
